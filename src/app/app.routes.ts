996b071367bdcf1ed65ebd09668957137a85008b
saxjax <saxjax@saxjax.dk>
2023-12-29 15:55:41 +0100

    initial version

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {MusicPageComponent} from "./music-page/music-page.component";
+import {ProductsPageComponent} from "./products-page/products-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'music', component:MusicPageComponent},
+  {path:'products', component:ProductsPageComponent}
+];
