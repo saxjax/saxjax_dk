fa24794ecfd9d4ad03cf4962a874b4b0725545d0
saxjax <saxjax@saxjax.dk>
2024-02-06 22:18:40 +0100

    using @for and @defer

--- src/app/pages/music-page/music-page.component.ts
+++ src/app/pages/music-page/music-page.component.ts
@@ -5,17 +5,31 @@ import { DescriptionAfterColonPipe } from '../../../pipes/description-after-colo
 import { NgFor } from '@angular/common';
 import { ContentPageComponent } from "../../common/content-page/content-page.component";
 import { ColorScheme } from '../../model/ColorScheme';
+import { TrustPipe } from '../../../pipes/trust.pipe';
 @Component({
     selector: 'music-page',
     standalone: true,
     templateUrl: './music-page.component.html',
     styleUrl: './music-page.component.scss',
-    imports: [NgFor, LinkFromParenthesisPipe, TitleFromSquareBracketsPipe, DescriptionAfterColonPipe, ContentPageComponent]
+    imports: [NgFor, LinkFromParenthesisPipe, TitleFromSquareBracketsPipe, DescriptionAfterColonPipe, TrustPipe, ContentPageComponent]
 })
 export class MusicPageComponent {
   @Input() title = "Orkestre som jeg har medvirket i:"
   @Input() bodyText?:string
   readonly ColorScheme = ColorScheme;
+  urls = [
+    "https://www.youtube.com/embed/DfdKn2OPxdU?si=_dLv2yLjnDZdp_3b",
+    "https://www.youtube.com/embed/AN4mBsFvv-M?si=YGwToGooBPG38CNA&amp;start=387",
+    "https://www.youtube.com/embed/Hz-r6fUDMhY?si=fGLj3TTy-gwqu-CY",
+    "https://www.youtube.com/embed/JHV9rtuYw_w?si=GshIh9v9zGLM5FYR&amp;start=115",
+    "https://www.youtube.com/embed/FLtRNflt0Ec?si=hL8v0dszwIRJzzrW&amp;start=75",
+    "https://www.youtube.com/embed/GTfFPsZRD0k?si=viJ8Sd9AgDob7jma&amp;start=895",
+    "https://www.youtube.com/embed/LsGLLy5C2mg?si=L70wMMqtr-whIuhT&amp;start=154",
+    "https://www.youtube.com/embed/NgdTN2xQ8Yg?si=auu1Rc8Jp88Vvt2u",
+    "https://embed.music.apple.com/dk/album/tango-vista-vinterjazz-2010/945138776",
+    "https://www.reverbnation.com/widget_code/html_widget/artist_405671?widget_id=55&pwc[included_songs]=1&context_type=page_object",
+    "https://open.spotify.com/embed/artist/1uuhruxGCOa1hTI7i9fMHx?utm_source=generator",
+  ]
 
 
   links = ["[Saxjax Lunatic Orchestra]()::Alternativ folke jazz::",
