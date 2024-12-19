019286ad072c8cbdf4d9bafad4deb69336d13ee0
saxjax <saxjax@saxjax.dk>
2024-12-19 21:16:17 +0100

    convex hull working

--- src/pipes/algorithms/convexHull.ts
+++ src/pipes/algorithms/convexHull.ts
@@ -1,36 +1,60 @@
-import { Point } from "../../app/model/point"
+import { Point } from "../../app/model/point";
 
-export const convexHull = (points:Point[]):Point[] => {
-  console.log("convexHull not implemented")
-  let xsortedPoints = [...points]
-  let ysortedPoints = [...points]
+// Helper function to calculate the cross product of three points (O, A, B)
+const crossProduct = (O: Point, A: Point, B: Point): number => {
+    return (A.x - O.x) * (B.y - O.y) - (A.y - O.y) * (B.x - O.x);
+};
 
-  const compareXFn = (a:Point,b:Point)=>{
-    if(a.x>b.x){
-      return -1
+// Convex Hull function using Graham's scan
+export const convexHull = (points: Point[]): Point[] => {
+    if (points.length < 3) {
+        console.log("Convex hull not applicable: fewer than 3 points");
+        return points;
     }
-    if(a.x<b.x){
-      return 1
-    }
-    return 0
-  }
 
-  const compareYFn = (a:Point,b:Point)=>{
-    if(a.y>b.y){
-      return -1
-    }
-    if(a.y<b.y){
-      return 1
-    }
-    return 0
-  }
+    // Sort points by x-coordinate, then by y-coordinate
+    const sortedPoints = [...points].sort((a, b) => {
+        if (a.x !== b.x) return a.x - b.x;
+        return a.y - b.y;
+    });
 
-  xsortedPoints.sort(compareXFn)
-  const xMax = xsortedPoints[xsortedPoints.length - 1]
-  const xMin = xsortedPoints[0]
-  ysortedPoints.sort(compareYFn)
-  const yMax = ysortedPoints[ysortedPoints.length - 1]
-  const yMin = ysortedPoints[0]
+    // Build the lower hull
+    const lowerHull: Point[] = [];
+    for (const point of sortedPoints) {
+        while (
+            lowerHull.length >= 2 &&
+            crossProduct(
+                lowerHull[lowerHull.length - 2],
+                lowerHull[lowerHull.length - 1],
+                point
+            ) <= 0
+        ) {
+            lowerHull.pop();
+        }
+        lowerHull.push(point);
+    }
 
-  return points
-}
+    // Build the upper hull
+    const upperHull: Point[] = [];
+    for (let i = sortedPoints.length - 1; i >= 0; i--) {
+        const point = sortedPoints[i];
+        while (
+            upperHull.length >= 2 &&
+            crossProduct(
+                upperHull[upperHull.length - 2],
+                upperHull[upperHull.length - 1],
+                point
+            ) <= 0
+        ) {
+            upperHull.pop();
+        }
+        upperHull.push(point);
+    }
+
+    // Remove the last point of each half because it's repeated at the start of the other half
+    upperHull.pop();
+    lowerHull.pop();
+
+    // Concatenate lower and upper hull to get the full convex hull
+    return lowerHull.concat(upperHull);
+};
