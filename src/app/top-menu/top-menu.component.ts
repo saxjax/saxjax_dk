367f89a4cb6cfdfd0de16ebf3bd1bf3ec6cddade
saxjax <saxjax@saxjax.dk>
2024-12-29 13:45:44 +0100

    using :enter animation to animate the initial menu animation, then i use state to manage menu animations on hover and pageselection

--- src/app/top-menu/top-menu.component.ts
+++ src/app/top-menu/top-menu.component.ts
@@ -4,10 +4,13 @@ import { RouterLink, RouterLinkActive } from "@angular/router";
 
 const hidden = { opacity:0.5, height:'5%',transform: 'translateY(-10%)' }
 const active = { opacity:1, height: '20%', transform: 'translateY(0%)' }
-const dorment = { opacity:0.5, height:'10%', transform: 'translateY(-10%)' }
+const dorment = { opacity: 0.5, height: '10%', transform: 'translateY(-10%)' }
+// const pageSelected = { opacity: 0.5, height: '5%', 'min-height':'100px'}
+// const noPageSelected = {opacity:1, height:'100%', 'min-height':'200px'}
+
 const timingIn = '1s ease-in'
 const timingToActive = '0.3s ease-in'
-const timingToDorment = '0.1s ease-out'
+const timingToDorment = '0.5s ease-out'
 
 @Component({
     selector: 'top-menu',
@@ -19,18 +22,25 @@ const timingToDorment = '0.1s ease-out'
   styleUrl: './top-menu.component.scss',
   animations: [
     trigger("showMenu", [
-      // transition(':enter', [style(hidden),
-      //   animate(timingIn, style(active))
-      // ]),
+      transition(':enter', [style(hidden),
+        animate(timingIn, style(active))
+      ]),
       state('initial',style(hidden)),
       state('closed',style(dorment)),
       state('open', style(active)),
       transition('initial => open', [animate(timingIn)]),
       transition('closed => open', [animate(timingToActive)]),
       transition('open => closed', [animate(timingToDorment)]),
-      ])
-    ]
+    ]),
+  //     trigger('pageSelected', [
+  //       state('pageSelected', style(pageSelected)),
+  //       state('noPageSelected', style(noPageSelected)),
+  //       transition('pageSelected => noPageSelected', [animate(timingIn)])
+  // ])
+
+  ],
 })
 export class TopMenuComponent {
-  protected menuState: 'open' | 'closed' | 'initial' = 'initial';
+  protected menuState: 'open' | 'closed' | 'initial' = 'open';
+  protected pageSelection:'pageSelected' | 'noPageSelected' = 'noPageSelected'
 }
