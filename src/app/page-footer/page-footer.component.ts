97517ade463b91813d825c8fa45bd9c3bd50b47f
saxjax <saxjax@saxjax.dk>
2024-12-29 14:26:28 +0100

    animating footer first attempt

--- src/app/page-footer/page-footer.component.ts
+++ src/app/page-footer/page-footer.component.ts
@@ -3,6 +3,7 @@ import {TlfFieldComponent} from "./tlf-field/tlf-field.component";
 import {NameFieldComponent} from "./name-field/name-field.component";
 import {MailFieldComponent} from "./mail-field/mail-field.component";
 import {AddressFieldComponent} from "./address-field/address-field.component";
+import { animate, stagger, state, style, transition, trigger, query } from '@angular/animations';
 
 @Component({
     selector: 'page-footer',
@@ -13,8 +14,21 @@ import {AddressFieldComponent} from "./address-field/address-field.component";
         AddressFieldComponent
     ],
     templateUrl: './page-footer.component.html',
-    styleUrl: './page-footer.component.scss'
+  styleUrl: './page-footer.component.scss',
+  animations: [
+    trigger('showInfo', [
+      // state('showInfo', style({ opacity:1, height: '100%', scaleY:1 })),
+      // state('hideInfo', style({ opacity:0.1, height: '10%', scaleY:0.1 })),
+      transition('* <=> *', [
+        query(':enter', [
+          style({ transform: 'translateY(-120%)' }),
+          animate('0.5s ease-in', style({ transform: 'translateY(0%)' }))
+        ])
+      ])
+    ])
+  ]
 })
 export class PageFooterComponent {
+  showInfoState: 'showInfo'|'hideInfo' = 'hideInfo'
 
 }
