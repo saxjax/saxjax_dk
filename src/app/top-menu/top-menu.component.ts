88d409adb778cc6b9bb7b7b28f65c7f9ea548466
saxjax <saxjax@saxjax.dk>
2024-12-29 13:18:23 +0100

    using state animation on menu

--- src/app/top-menu/top-menu.component.ts
+++ src/app/top-menu/top-menu.component.ts
@@ -2,10 +2,10 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from "@angular/router";
 
-const hidden = { transform: 'translateX(-120%)' }
-const active = { transform: 'translateX(0%) translateY(0%)' }
-const dorment = { transform: 'translateY(-10%)' }
-const timingIn = '0.3s ease-in'
+const hidden = { opacity:0.5, height:'5%',transform: 'translateY(-10%)' }
+const active = { opacity:1, height: '20%', transform: 'translateY(0%)' }
+const dorment = { opacity:0.5, height:'10%', transform: 'translateY(-10%)' }
+const timingIn = '1s ease-in'
 const timingToActive = '0.3s ease-in'
 const timingToDorment = '0.1s ease-out'
 
@@ -19,11 +19,11 @@ const timingToDorment = '0.1s ease-out'
   styleUrl: './top-menu.component.scss',
   animations: [
     trigger("showMenu", [
-      transition(':enter', [style(hidden),
-        animate('0.3s ease-in', style(active))
-      ]),
-        state('initial',style(hidden)),
-        state('closed',style(dorment)),
+      // transition(':enter', [style(hidden),
+      //   animate(timingIn, style(active))
+      // ]),
+      state('initial',style(hidden)),
+      state('closed',style(dorment)),
       state('open', style(active)),
       transition('initial => open', [animate(timingIn)]),
       transition('closed => open', [animate(timingToActive)]),
