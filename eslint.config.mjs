0d294cb706c5822000351c2a5fdc301399bbf8a8
saxjax <saxjax@saxjax.dk>
2025-01-25 00:25:47 +0100

    prettier setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import angular from "angular-eslint"
+import eslintConfigPrettier from "eslint-config-prettier";
+
 
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -9,4 +12,7 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  ...tseslint.configs.stylistic,
+  ...angular.configs.tsRecommended,
+  ...eslintConfigPrettier,
 ];
